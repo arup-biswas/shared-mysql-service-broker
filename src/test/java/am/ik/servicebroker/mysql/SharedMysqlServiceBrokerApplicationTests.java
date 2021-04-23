f7a0ade321d27ae34f69813f146c6f129fbd9478
root <root@arup-centos7.sjinternal.com>
2021-04-23 09:55:12 -0700

    Test git access

--- src/test/java/am/ik/servicebroker/mysql/SharedMysqlServiceBrokerApplicationTests.java
+++ src/test/java/am/ik/servicebroker/mysql/SharedMysqlServiceBrokerApplicationTests.java
@@ -1,5 +1,5 @@
 package am.ik.servicebroker.mysql;
-
+/* Testing git access */
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
